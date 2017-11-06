6aa7cd886c4f17f9d852c00d99ec391d03b5aa95
dewadhitya <dewa.adhitya21@gmail.com>
2017-11-06 23:37:37 +0800

    Commit

--- app/src/main/java/praktikum/progmob14/InputActivity.java
+++ app/src/main/java/praktikum/progmob14/InputActivity.java
@@ -2,6 +2,7 @@ package praktikum.progmob14;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -44,16 +45,49 @@ public class InputActivity extends AppCompatActivity {
         simpanHasil.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String nama = String.valueOf(inputNama.getText().toString());
-                String alamat = String.valueOf(inputAlamat.getText().toString());
-
-                int gender = jenisKelamin.getCheckedRadioButtonId();
-                RadioButton jenis_kelamin = (RadioButton) findViewById(gender);
-                String jKelamin = String.valueOf(jenis_kelamin.getText().toString());
-
-                //// TODO: 11/6/2017 Ngambil hasil string dari Checkbox
-                //// TODO: 11/6/2017 Ngambil hasil string dari Seekbar
+                showDialog();
             }
         });
+    }
+
+    //Menampilkan Alert Dialog
+    private void showDialog(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        //Set Title Dialog
+        alertDialogBuilder.setTitle("Hasil");
+
+        String nama = String.valueOf(inputNama.getText().toString());
+        String alamat = String.valueOf(inputAlamat.getText().toString());
+
+        int gender = jenisKelamin.getCheckedRadioButtonId();
+        RadioButton jenis_kelamin = (RadioButton) findViewById(gender);
+        String jKelamin = String.valueOf(jenis_kelamin.getText().toString());
+
+        //// TODO: 11/6/2017 Ngambil hasil string dari Checkbox
+        String hobi = "";
+        if(hbJogging.isChecked()){
+            hobi+="Jogging";
+        }
+        if(hbBersepeda.isChecked()){
+            hobi+="Bersepeda";
+        }
+        if(hbMembaca.isChecked()){
+            hobi+="Membaca";
+        }
+        if(hbMenari.isChecked()){
+            hobi+="Menari";
+        }
+        if(hbMelukis.isChecked()){
+            hobi+="Melukis";
+        }
+        if(hbBermain.isChecked()){
+            hobi+="Bermain";
+        }
+
+        //// TODO: 11/6/2017 Ngambil hasil string dari Seekbar
+
+        //Set Pesan Dialog
+        alertDialogBuilder
     }
 }
