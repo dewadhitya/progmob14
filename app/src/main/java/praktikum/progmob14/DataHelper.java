840ec2a5c76f6bce53015ed6b3d4b9b13accb80d
dewadhitya <dewa.adhitya21@gmail.com>
2017-11-18 21:14:56 +0800

    Modul III

--- app/src/main/java/praktikum/progmob14/DataHelper.java
+++ app/src/main/java/praktikum/progmob14/DataHelper.java
@@ -1,9 +1,9 @@
 package praktikum.progmob14;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
 
 /**
  * Created by Dewa Adhitya on 11/14/2017.
@@ -12,22 +12,38 @@ import android.util.Log;
 public class DataHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "db_kuisioner.db";
     private static final int DATABASE_VERSION = 1;
-    public DataHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    private static final String query_table = "CREATE TABLE IF NOT EXISTS tb_kuisioner(id_kuisioner integer primary key autoincrement, nama text null, alamat, text null, aktivitas text null);";
+    private static final String query_hapus_table = "DROP TABLE IF EXISTS query_table";
+
+    public DataHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         //Membuat Tabel
-        String sql = "create table tb_kuisioner(id_kuisioner integer primary key, nama text null, alamat, text null, aktivitas text null);";
-        Log.d("Data","onCreate: "+sql);
-        sqLiteDatabase.execSQL(sql);
-        sql = "insert into tb_kuisioner(id_kuisioner, nama, alamat, aktivitas) values ('1','Adhit','Jl. Mertayasa','setiap hari');";
-        sqLiteDatabase.execSQL(sql);
+//        String sql = "create table tb_kuisioner(id_kuisioner integer primary key, nama text null, alamat, text null, aktivitas text null);";
+//        Log.d("Data","onCreate Database: "+sql);
+//        sqLiteDatabase.execSQL(sql);
+//        sql = "insert into tb_kuisioner(nama, alamat, aktivitas) values ('Adhit','Jl. Mertayasa','setiap hari');";
+//        sqLiteDatabase.execSQL(sql);
+        sqLiteDatabase.execSQL(query_table);
+        System.out.println("Tabel sudah dibuat");
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int versi_lama, int versi_baru1) {
+        sqLiteDatabase.execSQL(query_hapus_table);
+        onCreate(sqLiteDatabase);
+    }
 
+    public void insert_data(String nama, String alamat, String aktivitas){
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("nama", nama);
+        values.put("alamat", alamat);
+        values.put("aktivitas", aktivitas);
+        database.insert("tb_kuisioner", null, values);
+        database.close();
     }
 }
--- app/src/main/java/praktikum/progmob14/OutputActivity.java
+++ app/src/main/java/praktikum/progmob14/OutputActivity.java
@@ -1,6 +1,5 @@
 package praktikum.progmob14;
 
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -33,6 +32,7 @@ public class OutputActivity extends AppCompatActivity {
 
         //Inisialisasi  Variabel Button ke Database
         simpanDatabase = (Button) findViewById(R.id.simpan_database);
+        //dbHelper = new DataHelper(OutputActivity.this);
 
         //Mengambil data dari InputActivity dan menampilkannya
         outputNama.setText(getIntent().getStringExtra("nama"));
@@ -45,8 +45,16 @@ public class OutputActivity extends AppCompatActivity {
         simpanDatabase.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                SQLiteDatabase db = dbHelper.getWritableDatabase();
-                db.execSQL("insert into tb_kuisioner('nama', 'alamat', 'aktivitas') values('outputNama','outputAlamat','aktivitas');");
+                //SQLiteDatabase db = dbHelper.getWritableDatabase();
+                final String nama = String.valueOf(outputNama.getText().toString());
+                final String alamat = String.valueOf(outputAlamat.getText().toString());
+                final String haktivitas = String.valueOf(aktivitas.getText().toString());
+                //db.execSQL("insert into tb_kuisioner('nama', 'alamat', 'aktivitas') values('outputNama','outputAlamat','aktivitas');");
+                //db.execSQL("insert into tb_kuisioner('nama','alamat','aktivitas') values('"+nama+"','"+alamat+"','"+haktivitas+"')");
+                //finish();
+                DataHelper dataHelper = new DataHelper(OutputActivity.this);
+                dataHelper.insert_data(nama,alamat,haktivitas);
+                Toast.makeText(OutputActivity.this, "Data telah disimpan di Database", Toast.LENGTH_LONG).show();
             }
         });
     }
