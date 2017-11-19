3d61a8cb19f8ab2dfb36592d218e0fad01f9e73d
dewadhitya <dewa.adhitya21@gmail.com>
2017-11-19 13:15:46 +0800

    Modul III

--- app/src/main/java/praktikum/progmob14/DataHelper.java
+++ app/src/main/java/praktikum/progmob14/DataHelper.java
@@ -10,9 +10,10 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 
 public class DataHelper extends SQLiteOpenHelper {
+    //Inisialisasi Database
     private static final String DATABASE_NAME = "db_kuisioner.db";
     private static final int DATABASE_VERSION = 1;
-    private static final String query_table = "CREATE TABLE IF NOT EXISTS tb_kuisioner(id_kuisioner integer primary key autoincrement, nama text null, alamat, text null, aktivitas text null);";
+    private static final String query_table = "CREATE TABLE IF NOT EXISTS tb_kuisioner(id_kuisioner integer primary key autoincrement, nama text null, alamat text null, aktivitas text null);";
     private static final String query_hapus_table = "DROP TABLE IF EXISTS query_table";
 
     public DataHelper(Context context) {
@@ -22,21 +23,18 @@ public class DataHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         //Membuat Tabel
-//        String sql = "create table tb_kuisioner(id_kuisioner integer primary key, nama text null, alamat, text null, aktivitas text null);";
-//        Log.d("Data","onCreate Database: "+sql);
-//        sqLiteDatabase.execSQL(sql);
-//        sql = "insert into tb_kuisioner(nama, alamat, aktivitas) values ('Adhit','Jl. Mertayasa','setiap hari');";
-//        sqLiteDatabase.execSQL(sql);
         sqLiteDatabase.execSQL(query_table);
         System.out.println("Tabel sudah dibuat");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int versi_lama, int versi_baru1) {
+        //Jika database sudah ada maka akan me-Replace
         sqLiteDatabase.execSQL(query_hapus_table);
         onCreate(sqLiteDatabase);
     }
 
+    //Method query insert ke tb_kuisioner
     public void insert_data(String nama, String alamat, String aktivitas){
         SQLiteDatabase database = this.getWritableDatabase();
         ContentValues values = new ContentValues();
