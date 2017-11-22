9261516a0feac350c9c54ef5f3a729b030712235
dewadhitya <dewa.adhitya21@gmail.com>
2017-11-22 13:32:52 +0800

    Modul 1 & Modul 2

--- app/src/main/java/praktikum/progmob14/InputActivity.java
+++ app/src/main/java/praktikum/progmob14/InputActivity.java
@@ -6,14 +6,12 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
-import android.widget.Toast;
 
 import com.rengwuxian.materialedittext.MaterialEditText;
 
@@ -33,10 +31,6 @@ public class InputActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.input_form);
-
-        //MODUL 2 -- Untuk mengetahui cek Status Lifecycle pada Pesan Log
-        Log.d(status, "The onCreate() event");
-        //MODUL 2 -- END
 
         //Inisialisasi
         inputNama = (MaterialEditText) findViewById(R.id.input_nama);
@@ -152,43 +146,6 @@ public class InputActivity extends AppCompatActivity {
 
         //Menampilkan Alert Dialog
         alertDialog.show();
-    }
-
-    //MODUL 2 --
-    //Untuk mengetahui status dari Lifecycle di Pesan Log
-    String status = "Android: ";
-    /**Method ini dipanggil ketika activity sudah terlihat pada user. */
-    @Override
-    protected void onStart(){
-        super.onStart();
-        Log.d( status,"The onStart() event");
-    }
-    /**Method ini dipanggil ketika activity mulai berinteraksi dengan user.*/
-    @Override
-    protected void onResume(){
-        super.onResume();
-        Log.d(status, "The onResume() event");
-    }
-    /**Method ini Dipanggil ketika activity berhenti sementara tidak menerima inputan user
-     dan tidak mengeksekusi kode apapun.*/
-    @Override
-    protected void onPause(){
-        super.onPause();
-        Log.d(status,"The onPause() event ");
-        Toast.makeText(getApplicationContext(), "On Pause Method Applied in InputActivity", Toast.LENGTH_LONG).show();
-    }
-    /**Method ini dipanggil ketika activity sudah tidak terlihat pada user.*/
-    @Override
-    protected void onStop(){
-        super.onStop();
-        Log.d(status,"The onStop() event");
-    }
-    /**Method ini dipanggil sebelum sebuah activity dimatikan (di destroy).*/
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-        Log.d(status,"The onDestroy() event");
-        Toast.makeText(getApplicationContext(), "On Destroy Method Applied", Toast.LENGTH_LONG).show();
     }
 
     //Dialog Exit
--- app/src/main/java/praktikum/progmob14/OutputActivity.java
+++ app/src/main/java/praktikum/progmob14/OutputActivity.java
@@ -3,6 +3,7 @@ package praktikum.progmob14;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -21,6 +22,10 @@ public class OutputActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.output_form);
+
+        //MODUL 2 -- Untuk mengetahui cek Status Lifecycle pada Pesan Log
+        Log.d(status, "The onCreate() event");
+        //MODUL 2 -- END
 
         ///MODUL 2 -- MENGAMBIL DATA DARI ACTIVITY INPUT
         //Inisialisasi Variabel pada output_from
@@ -53,6 +58,43 @@ public class OutputActivity extends AppCompatActivity {
                 Toast.makeText(OutputActivity.this, "Data telah disimpan di Database", Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    //MODUL 2 --
+    //Untuk mengetahui status dari Lifecycle di Pesan Log
+    String status = "Android: ";
+    /**Method ini dipanggil ketika activity sudah terlihat pada user. */
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d( status,"The onStart() event");
+    }
+    /**Method ini dipanggil ketika activity mulai berinteraksi dengan user.*/
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(status, "The onResume() event");
+    }
+    /**Method ini Dipanggil ketika activity berhenti sementara tidak menerima inputan user
+     dan tidak mengeksekusi kode apapun.*/
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(status,"The onPause() event ");
+        Toast.makeText(getApplicationContext(), "On Pause Method Applied in InputActivity", Toast.LENGTH_LONG).show();
+    }
+    /**Method ini dipanggil ketika activity sudah tidak terlihat pada user.*/
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(status,"The onStop() event");
+    }
+    /**Method ini dipanggil sebelum sebuah activity dimatikan (di destroy).*/
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(status,"The onDestroy() event");
+        Toast.makeText(getApplicationContext(), "On Destroy Method Applied", Toast.LENGTH_LONG).show();
     }
 
     //DoubleTap2Back --
